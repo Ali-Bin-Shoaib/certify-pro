0f7be200894939c5b3a37bcd4b373cff0184863f
Ali Bin Shoaib <124885897+Ali-Bin-Shoaib@users.noreply.github.com>
2025-09-10 20:41:00 +0300

    Add Content Security Policy meta tag to enhance security

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <!-- SEO Meta Tags -->
     <meta name="description"
